9473618af16c4fbf178d55662d4c7643623151fd
Maria <msadovnikov904@gmail.com>
2024-02-18 03:24:13 +0800

    deploy 18/02

---
+++ assets/CreateProject-BPP7VzIG.js
@@ -0,0 +1,1 @@
+import{_ as e,a as c,b as r}from"./index-Cw77O9yV.js";const t={};function n(o,a){return c(),r("div",null,"Тут создание проекта")}const _=e(t,[["render",n]]);export{_ as default};
---
+++ assets/LastTransaction-SnwRmwX7.js
@@ -0,0 +1,1 @@
+import{_ as e,a as c,b as n}from"./index-Cw77O9yV.js";const t={};function a(r,o){return c(),n("div",null,"Последние транзакции")}const _=e(t,[["render",a]]);export{_ as default};
