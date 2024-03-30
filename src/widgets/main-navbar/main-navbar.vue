00c6a7f2b451a280c24f8b671677610ea5bd815e
Maria <msadovnikov904@gmail.com>
2024-03-30 22:26:21 +0300

    deploy 30/03

--- src/widgets/main-navbar/main-navbar.vue
+++ src/widgets/main-navbar/main-navbar.vue
@@ -4,14 +4,6 @@
 
 <template>
   <div class="navbar">
-    <div class="navbar__item user">
-      <div class="user__avatar">
-        <img src="@/assets/img/test_avatar.png">
-      </div>
-      <div class="user__name">
-          Иванова И.И.
-      </div>
-    </div>
     <div class="navbar__item">
       <router-link class="navbar__item__last-operation" to="/last-transaction">
         <img src="@/assets/img/last_operation.png">
